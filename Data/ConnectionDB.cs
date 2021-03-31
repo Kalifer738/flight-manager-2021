8147ec54a2a8bdf3e0f74e4df318983063837d08
dakata23 <47471622+dakata23@users.noreply.github.com>
2021-04-01 02:33:34 +0300

    Add files via upload
    Database and there may be some problems

---
+++ Data/ConnectionDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class ConnectionDB : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+
+        public virtual DbSet<Reservation> Reservations { get; set; }
+
+        public virtual DbSet<Flight> Flights { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ContactsDb;");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+
+    }
+}
---
+++ Data/Entity/User.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Entity
+{
+    public class User
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// 
+
+
+        //required user's username for database
+        [Required]
+        public string UserName { get; set; }
+
+        //required user's password for database
+        [Required]
+        public string Password { get; set; }
+
+        //required user's email for database
+        [Required]
+        public string Email { get; set; }
+
+        //required user's first name for database
+        [Required]
+        public string FirstName { get; set; }
+
+        //required user's last name for database
+        [Required]
+        public string LastName { get; set; }
+
+        //primary key user's egn for database
+        public int EGN { get; set; }
+
+        //user's address for database
+        public string Address { get; set; }
+
+        //user's phone number for database
+        public int PhoneNumber { get; set; }
+
+        //user's role for database
+        public string Role { get; set; }
+    }
+}
