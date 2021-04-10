850033e86970c9e3313dc155864db058639d7ce5
Kalifer <kik8bgork@gmail.com>
2021-04-10 12:48:12 +0300

    dunno

--- flight-manager-2021/Controllers/LoginController.cs
+++ flight-manager-2021/Controllers/LoginController.cs
@@ -17,10 +17,12 @@ namespace flight_manager_2021.Controllers
     {
         private ILogger<LoginIndexViewModel> _logger;
         private readonly ConnectionDB _context;
+        private SignInManager<User> _signInManager;
 
-        public LoginController(ILogger<LoginIndexViewModel> logger)
+        public LoginController(ILogger<LoginIndexViewModel> logger, SignInManager<User> signInManager)
         {
             _logger = logger;
+            _signInManager = signInManager;
             _context = new ConnectionDB();
         }
 
@@ -44,14 +46,28 @@ namespace flight_manager_2021.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Login(LoginIndexViewModel model)
+        public async Task<IActionResult> Login(string userName, string password)
         {
+            string returnUrl = Url.Content("~/");
+
             if (ModelState.IsValid)
             {
-                User user = await _context.Users.FirstAsync(x => x.UserName == model.UserName && x.Password == model.Password);
-            }
+                User user = await _context.Users.FirstAsync(x => x.UserName == userName && x.Password == password);
+                var result = await _signInManager.PasswordSignInAsync(userName, password, false, false);
 
-            return View(model);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User Logged in");
+                    return LocalRedirect(returnUrl);
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View();
+                }
+            }
+            
+            return View();
         }
     }
 }
